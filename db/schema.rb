d71dd904f1defca4cf2c16331bb30b5206544881
kiwasaki06 <jamjamjamjam666@gmail.com>
2019-07-06 15:16:00 +0900

    fix-comment

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20190706015334) do
   create_table "comments", force: :cascade do |t|
     t.string "body"
     t.integer "post_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
